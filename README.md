62ebacbf7b9ef4cce455a859f01b0429faca3409
mykeeouellet <mykee.o@icloud.com>
2020-04-09 21:18:47 -0700

    minor changes

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 ROCKET ELEVATOR FOUNDATION // Week 9 Odyssey Program 
 
-Consolidation Week. 
+Zendesk API // Usage
+
+This Api will be triggered each time an employee creates an intervention on 
+the website. It will make a 'Ticket' with all the information the employee
+enters on our Zendesk account for each intervention we receive.
+
+To Use it will to first login to our Zendesk account.
+
+    - URL : https://rocketelevators5361.zendesk.com/agent/welcome
+    - Username : mykeeouellet@hotmail.com
+    - Password : Mykee@123
+
+Then you will need to sign-in as an employee on the rocket elevators
+website. You will then be able to navigate to the dropdown menu under
+the CONTACT section of the home page and select "Intervention Form". 
+
+This will lead you there => (http://rocketelevators.ca/interventions/new).
+
+To generate an intervention just fullfill the selectors and click create 
+at the bottom of the form. Now you just need to navigate to the Zendesk 
+panel and your newly created form will be there as a ticket.
+
+REST API // Usage
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,28 @@ p "Lead:"
     puts lead.inspect
 end
 
+# Generate employee user and account
+# u = User.create!(username: 'Nicolasgenest', email: 'nicolas.genest@codeboxx.biz', company: 'Codeboxx1', password: 'password', password_confirmation: 'password', role_id: 3) if Rails.env.development?
+# e = Employee.create!(name: 'Genest', firstName: 'Nicolas', title: 'Comm Rep', email: 'nicolas.genest@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Nadyafortier', email: 'nadya.fortier@codeboxx.biz', company: 'Codeboxx2', password: 'password', password_confirmation: 'password', role_id: 1) if Rails.env.development?
+# e = Employee.create!(name: 'Fortier', firstName: 'Nadya', title: 'Director', email: 'nadya.fortier@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Martinchantal', email: 'martin.chantal@codeboxx.biz', company: 'Codeboxx3', password: 'password', password_confirmation: 'password', role_id: 1) if Rails.env.development?
+# e = Employee.create!(name: 'Chantal', firstName: 'Martin', title: 'Director Assistant', email: 'martin.chantal@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Mathieuhoude', email: 'mathieu.houde@codeboxx.biz', company: 'Codeboxx4', password: 'password', password_confirmation: 'password', role_id: 1) if Rails.env.development?
+# e = Employee.create!(name: 'Houde', firstName: 'Mathieu', title: 'Captain', email: 'mathieu.houde@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Remigagnon', email: 'remi.gagnon@codeboxx.biz', company: 'Codeboxx5', password: 'password', password_confirmation: 'password', role_id: 3) if Rails.env.development?
+# e = Employee.create!(name: 'Gagnon', firstName: 'Remi', title: 'Engineer', email: 'remi.gagnon@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Mathieulefrancois', email: 'mathieu.lefrancois@codeboxx.biz', company: 'Codeboxx6', password: 'password', password_confirmation: 'password', role_id: 3) if Rails.env.development?
+# e = Employee.create!(name: 'Lefrancois', firstName: 'Mathieu', title: 'Engineer', email: 'mathieu.lefrancois@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Sergesavoie', email: 'serge.savoie@codeboxx.biz', company: 'Codeboxx7', password: 'password', password_confirmation: 'password', role_id: 3) if Rails.env.development?
+# e = Employee.create!(name: 'Savoie', firstName: 'Serge', title: 'Engineer', email: 'serge.savoie@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Davidboutin', email: 'david.boutin@codeboxx.biz', company: 'Codeboxx8', password: 'password', password_confirmation: 'password', role_id: 3) if Rails.env.development?
+# e = Employee.create!(name: 'Boutin', firstName: 'David', title: 'Engineer', email: 'david.boutin@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Mathieulortie', email: 'mathieu.lortie@codeboxx.biz', company: 'Codeboxx9', password: 'password', password_confirmation: 'password', role_id: 3) if Rails.env.development?
+# e = Employee.create!(name: 'Lortie', firstName: 'Mathieu', title: 'Engineer', email: 'mathieu.lortie@codeboxx.biz', user: u) if Rails.env.development?
+# u = User.create!(username: 'Thomascarrier', email: 'thomas.carrier@codeboxx.biz', company: 'Codeboxx10', password: 'password', password_confirmation: 'password', role_id: 3) if Rails.env.development?
+# e = Employee.create!(name: 'Carrier', firstName: 'Thomas', title: 'Engineer', email: 'thomas.carrier@codeboxx.biz', user: u) if Rails.env.development?
+
 #seeding the users talbe
 p "users:"
 50.times do
